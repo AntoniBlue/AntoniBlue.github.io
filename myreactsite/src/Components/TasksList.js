965130619e79f879dab0991b34df1e2dcfd3f384
AntoniBlue <107003642+AntoniBlue@users.noreply.github.com>
2023-04-26 18:54:12 +0300

    set up Constructor
    state in class component
    cant use hooks in class comp

--- myreactsite/src/Components/TasksList.js
+++ myreactsite/src/Components/TasksList.js
@@ -2,12 +2,24 @@ import React from 'react';
 import TaskItem from './TaskItem';
 
 class TaskList extends React.Component {
+    constructor(prop) {
+        super(props)
+
+        this.state = {
+            tasks: [
+                'Task 1',
+                'Task 2',
+                'Task 3',
+            ],
+            filter: [
+                'all',
+            ]
+        }
+    }
     render() {
         return (
             <ul>
-                <TaskItem title={'Task 1'} />
-                <TaskItem title={'Task 2'} />
-                <TaskItem title={'Task 3'} />
+                {this.state.tasks.map(x => <TaskItem title={x} />)}
             </ul>
         );
     }
